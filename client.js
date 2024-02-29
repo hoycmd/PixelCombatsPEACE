536de885252295bdb7374b5e5b7a7d4683ca936b
PixelCombats2 <145314654+hoycmd@users.noreply.github.com>
2024-02-29 17:58:35 +0700

    Update client.js

--- client.js
+++ client.js
@@ -58,11 +58,11 @@ Ui.getContext().Hint.Value = "Hint/BuildBase";
 // ������������ ���������
 var inventory = Inventory.GetContext();
 inventory.Main.Value = true;
-inventory.Mainfinity.Value = true;
+inventory.MainInfinity.Value = true;
+inventory.SecondaryInfinity.Value = true;
 inventory.Secondary.Value = true;
-inventory.Secondaryfinity.Value = true;
 inventory.Melee.Value = true;
-inventory.Explosivefinity.Value = true;
+inventory.ExplosiveInfinity.Value = true;
 inventory.Explosive.Value = true;
 inventory.Build.Value = true;
 inventory.BuildInfinity.Value = true;
